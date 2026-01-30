0f0d53d0f7b6935c419b627299c1a50769cdb986
nikesh2511 <nikeshmakizhini7@gmail.com>
2026-01-30 14:13:02 +0530

    Update problem_06.py

--- problem_06.py
+++ problem_06.py
@@ -3,6 +3,9 @@
 
 text = "Python"
 reversed_text = ""
-for i in range(len(text), 0, -1):
+
+# Start from the last index (len(text)-1) down to 0
+for i in range(len(text) - 1, -1, -1):
     reversed_text += text[i]
+
 print(f"Reversed: {reversed_text}")
